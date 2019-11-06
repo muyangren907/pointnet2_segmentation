a463b40746e511844af10896b05cb66cd0f82dea
muyangren907 <907097904@qq.com>
2019-11-06 22:28:19 +0800

    update

--- scannet/kitti_util.py
+++ scannet/kitti_util.py
@@ -8,10 +8,11 @@ import pprint
 import pickle
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-ROOT_DIR = BASE_DIR
+ROOT_DIR = os.path.dirname(BASE_DIR)
 DATA_DIR = os.path.join(ROOT_DIR, 'data')
 if not os.path.exists(DATA_DIR):
     os.makedirs(DATA_DIR)
+    print(os.path.join(DATA_DIR, 'training'))
 if not os.path.exists(os.path.join(DATA_DIR, 'training')):
     www_list = ['https://s3.eu-central-1.amazonaws.com/avg-kitti/data_object_velodyne.zip',
                 'https://s3.eu-central-1.amazonaws.com/avg-kitti/data_object_label_2.zip',
