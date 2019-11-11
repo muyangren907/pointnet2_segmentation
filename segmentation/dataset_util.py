a61fd79bc3796d44d472b7f79fe3a44f8e852b68
muyangren907 <907097904@qq.com>
2019-11-11 15:22:52 +0800

    update

--- segmentation/dataset_util.py
+++ segmentation/dataset_util.py
@@ -8,13 +8,16 @@ def deal_dataset(DATASET, DOWNLOADER, DATA_PATH):
         NUM_CLASSES = 21
         www = 'https://shapenet.cs.stanford.edu/media/scannet_data_pointnet2.zip'
         zipfile = os.path.basename(www)
-        if not os.path.exists(os.path.join(DATA_PATH, zipfile)):
-            if DOWNLOADER == 'wget':
-                os.system('wget %s' % www)
-            elif DOWNLOADER == 'aria2':
-                os.system('aria2c -x 15 -s 15 %s' % www)
-            os.system('mv %s %s' % (zipfile, DATA_PATH))
-        if not os.path.exists(os.path.join(DATA_PATH, 'scannet_train.pickle')):
+        train_file = os.path.join(DATA_PATH, 'scannet_train.pickle')
+        test_file = os.path.join(DATA_PATH, 'scannet_test.pickle')
+        if not (os.path.exists(train_file) and os.path.exists(test_file)):
+            if not os.path.exists(os.path.join(DATA_PATH, zipfile)):
+                if DOWNLOADER == 'wget':
+                    os.system('wget %s' % www)
+                elif DOWNLOADER == 'aria2':
+                    os.system('aria2c -x 15 -s 15 %s' % www)
+                os.system('mv %s %s' % (zipfile, DATA_PATH))
+        # if not os.path.exists(os.path.join(DATA_PATH, 'scannet_train.pickle')):
             unzipfile = os.path.join(DATA_PATH, zipfile)
             os.system('unzip -q %s -d %s' % (unzipfile, DATA_PATH))
             os.system('mv %s/* %s' % (os.path.join(DATA_PATH, 'data'), DATA_PATH))
