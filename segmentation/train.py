fc1352cc29fe37704d021bb513c398b30cdf16f0
muyangren907 <907097904@qq.com>
2019-11-13 15:35:18 +0800

    update

--- segmentation/train.py
+++ segmentation/train.py
@@ -353,7 +353,7 @@ def eval_one_epoch(sess, ops, test_writer):
                      ops['smpws_pl']: batch_smpw,
                      ops['is_training_pl']: is_training}
         # tmp print
-        print(batch_idx, feed_dict)
+        print(batch_idx)
         summary, step, loss_val, pred_val = sess.run([ops['merged'], ops['step'],
                                                       ops['loss'], ops['pred']], feed_dict=feed_dict)
         test_writer.add_summary(summary, step)
@@ -479,7 +479,7 @@ def eval_whole_scene_one_epoch(sess, ops, test_writer):
                      ops['smpws_pl']: batch_smpw,
                      ops['is_training_pl']: is_training}
         # tmp print
-        print(batch_idx, feed_dict)
+        print(batch_idx)
         summary, step, loss_val, pred_val = sess.run([ops['merged'], ops['step'],
                                                       ops['loss'], ops['pred']], feed_dict=feed_dict)
         test_writer.add_summary(summary, step)
