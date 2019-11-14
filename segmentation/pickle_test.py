4b22c7a39e4559d4a942ff488d06368815e46327
muyangren907 <907097904@qq.com>
2019-11-14 21:44:04 +0800

    update

--- segmentation/pickle_test.py
+++ segmentation/pickle_test.py
@@ -33,7 +33,7 @@ if __name__ == '__main__':
     print(scene_points_list[0].shape, semantic_labels_list[0].shape)
     print(scene_points_list[0])
     print(semantic_labels_list[0])
-    input('continue')
+    # input('continue')
     if DATASET == 'scannet':
         NUM_CLASSES = 21
     elif DATASET == 'kitti':
@@ -63,45 +63,45 @@ if __name__ == '__main__':
         #     break
     print(label_count)
     print(np.sum(label_count))
-    print()
-    a = np.array([1, 1, 2])
-    b = np.array([1, 2, 3])
-    c = a + b
-    print(a)
-    print(b)
-    print(c)
-    print(c - 2)
-    point_set_ini = np.array(
-        [
-            [7.2, 1, 2.9],
-            [8.4, 2, 1.4],
-            [3.2, 3.4, 2.5],
-            [2.9, 4.2, 1.3]
-        ]
-
-    )
-    print('point_set_ini')
-    print(point_set_ini)
-    coordmax = np.max(point_set_ini, axis=0)
-    # 获取(x,y,z)每一项的最小值，不一定为同一个点
-    coordmin = np.min(point_set_ini, axis=0)
-    print(coordmin)
-    print(coordmax)
-
-    step = 1.6
-    nsubvolume_x = np.ceil((coordmax[0] - coordmin[0]) / step).astype(np.int32)
-    nsubvolume_y = np.ceil((coordmax[1] - coordmin[1]) / step).astype(np.int32)
-    print(nsubvolume_x, nsubvolume_y)
-    for i in range(nsubvolume_x):
-        for j in range(nsubvolume_y):
-            print()
-            print(i, j)
-            # curmin = coordmin + [i * 1.5, j * 1.5, 0]
-            curmin = coordmin + [i * step, j * step, 0]
-            print('curmin', curmin)
-            # curmax = coordmin + [(i + 1) * 1.5, (j + 1) * 1.5, coordmax[2] - coordmin[2]]
-            curmax = coordmin + [(i + 1) * step, (j + 1) * step, coordmax[2] - coordmin[2]]
-            print('curmax', curmax)
-            # curchoice = np.sum((point_set_ini >= (curmin - 0.2)) * (point_set_ini <= (curmax + 0.2)), axis=1) == 3
-            sum = np.sum((point_set_ini >= (curmin - 0.2)) * (point_set_ini <= (curmax + 0.2)), axis=1)
-            print('sum', sum)
+    # print()
+    # a = np.array([1, 1, 2])
+    # b = np.array([1, 2, 3])
+    # c = a + b
+    # print(a)
+    # print(b)
+    # print(c)
+    # print(c - 2)
+    # point_set_ini = np.array(
+    #     [
+    #         [7.2, 1, 2.9],
+    #         [8.4, 2, 1.4],
+    #         [3.2, 3.4, 2.5],
+    #         [2.9, 4.2, 1.3]
+    #     ]
+    #
+    # )
+    # print('point_set_ini')
+    # print(point_set_ini)
+    # coordmax = np.max(point_set_ini, axis=0)
+    # # 获取(x,y,z)每一项的最小值，不一定为同一个点
+    # coordmin = np.min(point_set_ini, axis=0)
+    # print(coordmin)
+    # print(coordmax)
+    #
+    # step = 1.6
+    # nsubvolume_x = np.ceil((coordmax[0] - coordmin[0]) / step).astype(np.int32)
+    # nsubvolume_y = np.ceil((coordmax[1] - coordmin[1]) / step).astype(np.int32)
+    # print(nsubvolume_x, nsubvolume_y)
+    # for i in range(nsubvolume_x):
+    #     for j in range(nsubvolume_y):
+    #         print()
+    #         print(i, j)
+    #         # curmin = coordmin + [i * 1.5, j * 1.5, 0]
+    #         curmin = coordmin + [i * step, j * step, 0]
+    #         print('curmin', curmin)
+    #         # curmax = coordmin + [(i + 1) * 1.5, (j + 1) * 1.5, coordmax[2] - coordmin[2]]
+    #         curmax = coordmin + [(i + 1) * step, (j + 1) * step, coordmax[2] - coordmin[2]]
+    #         print('curmax', curmax)
+    #         # curchoice = np.sum((point_set_ini >= (curmin - 0.2)) * (point_set_ini <= (curmax + 0.2)), axis=1) == 3
+    #         sum = np.sum((point_set_ini >= (curmin - 0.2)) * (point_set_ini <= (curmax + 0.2)), axis=1)
+    #         print('sum', sum)
