e729f4b897712ad51ffebc52fd2076075ff41cc3
muyangren907 <907097904@qq.com>
2019-11-15 14:06:35 +0800

    update

--- segmentation/scene_dis.py
+++ segmentation/scene_dis.py
@@ -1,9 +1,9 @@
 import pickle
 import os
 import argparse
-# import numpy as np
+import numpy as np
 # import cupy as np
-import minpy as np
+# import minpy as np
 # pclpy only support on windows
 # import pclpy
 # from pclpy import pcl
