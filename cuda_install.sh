aa5c6337fd9f6388bdfe2ca1ec29f9aa90c1a256
muyangren907 <907097904@qq.com>
2019-11-20 20:30:49 +0800

    update

--- cuda_install.sh
+++ cuda_install.sh
@@ -2,7 +2,7 @@ apt install aria2 -y
 aria2c -x 15 -s 15 "https://developer.nvidia.com/compute/cuda/10.0/Prod/local_installers/cuda_10.0.130_410.48_linux"
 chmod +x cuda_10.0.130_410.48_linux
 ./cuda_10.0.130_410.48_linux
-echo -e '\nexport PATH="/usr/local/cuda-10.0/bin:$PATH"\nexport LD_LIBRARY_PATH="/usr/local/cuda-10.0/lib64:$LD_LIBRARY_PATH"'>>~/.bashrc
+echo -e '\nexport PATH="/usr/local/cuda-10.0/bin:$PATH"\nexport LD_LIBRARY_PATH="/usr/local/cuda-10.0/lib64:$LD_LIBRARY_PATH"' >>~/.bashrc
 source ~/.bashrc
-echo -e '\nexport PATH=$PATH:/usr/local/cuda-10.0/bin\nexport LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda-10.0/lib64\nexport LIBRARY_PATH=$LIBRARY_PATH:/usr/local/cuda-10.0/lib64'>>/etc/profile
+echo -e '\nexport PATH=$PATH:/usr/local/cuda-10.0/bin\nexport LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda-10.0/lib64\nexport LIBRARY_PATH=$LIBRARY_PATH:/usr/local/cuda-10.0/lib64' >>/etc/profile
 source /etc/profile
