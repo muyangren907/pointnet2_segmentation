123395ce90404537d969e1050b15e586afb9ff9d
muyangren907 <907097904@qq.com>
2019-11-21 22:57:19 +0800

    update

--- segmentation/pred.py
+++ segmentation/pred.py
@@ -278,7 +278,7 @@ def predict_one_epoch(sess, ops, train_writer):
 
         batch_data = batch_data.reshape(1, batch_data.shape[0], 3)
         batch_label = batch_label.reshape(1, batch_label.shape[0])
-        batch_smpw = np.ones(batch_label.shape[1])
+        batch_smpw = np.ones(batch_label.shape[1]).reshape(1,batch_label.shape[1])
 
         # Augment batched point clouds by rotation
         aug_data = provider.rotate_point_cloud_z(batch_data)
