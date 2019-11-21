4fa4254c959803ccb4c0c086da304edb8578c5b7
muyangren907 <907097904@qq.com>
2019-11-21 23:07:31 +0800

    update

--- segmentation/pred.py
+++ segmentation/pred.py
@@ -34,7 +34,7 @@ parser.add_argument('--model', type=str, default='pointnet2_sem_seg', help='Mode
 parser.add_argument('--model_load', type=str, default='kitti.ckpt', help='Model data file name')
 parser.add_argument('--log_dir', type=str, default='log', help='Log dir [default: log]')
 parser.add_argument('--num_point', type=int, default=8192, help='Point Number [default: 8192]')
-parser.add_argument('--max_epoch', type=int, default=201, help='Epoch to run [default: 201]')
+# parser.add_argument('--max_epoch', type=int, default=1, help='Epoch to run [default: 1]')
 # parser.add_argument('--batch_size', type=int, default=1, help='Batch Size during training [default: 1]')
 parser.add_argument('--learning_rate', type=float, default=0.001, help='Initial learning rate [default: 0.001]')
 parser.add_argument('--momentum', type=float, default=0.9, help='Initial learning rate [default: 0.9]')
@@ -52,7 +52,8 @@ EPOCH_CNT = 0
 # BATCH_SIZE = FLAGS.batch_size
 BATCH_SIZE = 1
 NUM_POINT = FLAGS.num_point
-MAX_EPOCH = FLAGS.max_epoch
+# MAX_EPOCH = FLAGS.max_epoch
+MAX_EPOCH = 1
 BASE_LEARNING_RATE = FLAGS.learning_rate
 GPU_INDEX = FLAGS.gpu
 MOMENTUM = FLAGS.momentum
